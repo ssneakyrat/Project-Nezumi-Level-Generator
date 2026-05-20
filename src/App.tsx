4f436d5d3a62b5a24cb7653e6ad1cefe5ca49682
ssneakyrat <59899200+ssneakyrat@users.noreply.github.com>
2026-05-20 22:00:48 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="container">
+      <h1>Hello World</h1>
+      <p>Welcome to Project-Nezumi-Level-Generator</p>
+    </main>
+  );
+}
+
+export default App;
